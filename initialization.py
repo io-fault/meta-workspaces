0bdee1004367d9a8fb1e5c5eb12db6243946d9d5
fault <critical@fault.io>
2021-07-17 01:22:42 -0700

    Limit initialization to a single context(v2 transition).

--- initialization.py
+++ initialization.py
@@ -9,31 +9,39 @@ directory_paths = [
 	'captures',
 ]
 
-intentions = [
-	'optimal',
-	'debug',
-	'coverage',
-	'profile',
-]
-
-def cc(route):
+def cc(route, intentions):
 	import system.factors.bin.initialize as init
 	import system.machine.bin.setup as sysi
 	import system.python.bin.setup as pyi
 
-	for i in intentions:
-		ccr = (route/i/'host').fs_alloc()
-		init.context(ccr, i, {}, set())
-		ctx = pyi.cc.Context.from_directory(ccr)
-		sysi.install(ccr, ctx, 'fault', ['CC', os.environ.get('CC') or '/usr/bin/cc'])
-		pyi.install(ccr, ctx, {})
+	ccr = None
+	ccr = (route).fs_alloc().fs_mkdir()
+	init.context(ccr, 'optimal', {}, set())
+	ctx = pyi.cc.Context.from_directory(ccr)
+	sysi.install(ccr, ctx, 'fault', ['CC', os.environ.get('CC') or '/usr/bin/cc'])
+	pyi.install(ccr, ctx, {})
 
 def directories(route):
 	(route).fs_mkdir()
 	for x in directory_paths:
 		(route@x).fs_mkdir()
 
-def root(wkenv):
+def root(wkenv, intentions, relevel):
 	route = wkenv.work_space_tooling.route
-	directories(route)
-	cc(route/'cc')
+
+	if relevel:
+		if relevel == 2:
+			directories(route)
+		elif relevel == 1:
+			# Overwrite inplace.
+			pass
+		else:
+			raise ValueError("invalid relevel") # 0, 1, or 2
+	else:
+		if route.fs_type() != 'void':
+			# Already exist and -rR not specified.
+			return -1
+		directories(route)
+
+	cc(route/'cc', intentions)
+	return len(intentions)
